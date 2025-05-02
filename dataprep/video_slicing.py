95a3880a48bfd41728b6f7038955d7597179f223
lukas <82029186+42lukas@users.noreply.github.com>
2025-05-02 13:10:59 +0200

    prepared trainings data

---
+++ dataprep/video_slicing.py
@@ -0,0 +1,36 @@
+import cv2
+import random
+
+raw_image_array = []
+val_array = []
+train_array = []
+
+# Load the video
+video_path = "dataprep/data.MOV"
+vidcap = cv2.VideoCapture(video_path)
+success,image = vidcap.read()
+
+frame_interval = 15
+count = 0
+
+
+while success:
+  if count % frame_interval == 0:
+    raw_image_array.append(image)      
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
+
+# split the data into training and validation sets
+random.shuffle(raw_image_array)
+split_index = int(len(raw_image_array) * 0.8)
+train_array = raw_image_array[:split_index]
+val_array = raw_image_array[split_index:]
+
+for i, image in enumerate(train_array):
+  cv2.imwrite(f"./dataset/images/train/train_{i:04d}.jpg", image)
+  print(f"Saved train image {i}.jpg")
+
+for i, image in enumerate(val_array):
+  cv2.imwrite(f"./dataset/images/val/val_{i:04d}.jpg", image)
+  print(f"Saved val image {i}.jpg")
